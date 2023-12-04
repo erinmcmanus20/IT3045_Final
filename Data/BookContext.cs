9a2afc933cab53dcb621b24c95aad89430709062
noahwardega <130424011+noahwardega@users.noreply.github.com>
2023-12-03 21:04:23 -0500

    Added Book data

---
+++ Data/BookContext.cs
@@ -0,0 +1,24 @@
+﻿
+using IT3045_Final.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT3045_Final.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Genre = "Classic", PublicationYear = 1925 },
+                new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Genre = "Fiction", PublicationYear = 1960 },
+                new Book { Id = 3, Title = "1984", Author = "George Orwell", Genre = "Dystopian", PublicationYear = 1949 },
+                new Book { Id = 4, Title = "The Hitchhiker's Guide to the Galaxy", Author = "Douglas Adams", Genre = "Science Fiction", PublicationYear = 1979 },
+                new Book { Id = 5, Title = "The Catcher in the Rye", Author = "J.D. Salinger", Genre = "Coming of Age", PublicationYear = 1951 }
+            );
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Data/BookContextDOA.cs
@@ -0,0 +1,93 @@
+﻿
+
+using System.Collections.Generic;
+using IT3045_Final.Interfaces;
+using IT3045_Final.Models;
+
+namespace IT3045_Final.Data
+{
+    public class BookContextDAO : IBookContextDAO
+    {
+        private BookContext _context;
+
+        public BookContextDAO(BookContext context)
+        {
+            _context = context;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public Book GetBookById(int id)
+        {
+            return _context.Books.Find(id);
+        }
+
+        public Book RemoveBookById(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+                return null;
+
+            _context.Books.Remove(book);
+            return book;
+        }
+
+        public Book UpdateBook(Book updatedBook)
+        {
+            var bookToUpdate = _context.Books.Find(updatedBook.Id);
+            if (bookToUpdate == null)
+                return null;
+
+            if (!string.IsNullOrEmpty(updatedBook.Title))
+                bookToUpdate.Title = updatedBook.Title;
+            if (!string.IsNullOrEmpty(updatedBook.Author))
+                bookToUpdate.Author = updatedBook.Author;
+            if (!string.IsNullOrEmpty(updatedBook.Genre))
+                bookToUpdate.Genre = updatedBook.Genre;
+
+            try
+            {
+                _context.Books.Update(bookToUpdate);
+                _context.SaveChanges();
+                return bookToUpdate;
+            }
+            catch (Exception)
+            {
+                // Handle exception
+                return null;
+            }
+        }
+
+        public Book AddBook(Book newBook)
+        {
+            var existingBook = _context.Books.FirstOrDefault(b => b.Title == newBook.Title && b.Author == newBook.Author);
+            if (existingBook != null)
+                return null; // Book already exists
+
+            try
+            {
+                var bookToAdd = new Book
+                {
+                    Title = newBook.Title,
+                    Author = newBook.Author,
+                    Genre = newBook.Genre,
+                    PublicationYear = newBook.PublicationYear
+                };
+
+                _context.Books.Add(bookToAdd);
+                _context.SaveChanges();
+                return bookToAdd;
+            }
+            catch (Exception)
+            {
+                // Handle exception
+                return null;
+            }
+        }
+    }
+}
+
+
