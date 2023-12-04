b6c386ece62375ca1bb9a0d4597b7ab482bf0122
noahwardega <130424011+noahwardega@users.noreply.github.com>
2023-12-03 20:57:13 -0500

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace IT3045_Final.Models
+{
+    public class Book
+    {
+     
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PublicationYear { get; set; }
+    }
+
+}
+
