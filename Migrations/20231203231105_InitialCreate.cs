1284998f2970e240e5ee92b3d0708bb1022fb570
noahwardega <130424011+noahwardega@users.noreply.github.com>
2023-12-03 21:07:52 -0500

    Add files via upload

---
+++ Migrations/20231203231105_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace IT3045Final.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublicationYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Genre", "PublicationYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, "F. Scott Fitzgerald", "Classic", 1925, "The Great Gatsby" },
+                    { 2, "Harper Lee", "Fiction", 1960, "To Kill a Mockingbird" },
+                    { 3, "George Orwell", "Dystopian", 1949, "1984" },
+                    { 4, "Douglas Adams", "Science Fiction", 1979, "The Hitchhiker's Guide to the Galaxy" },
+                    { 5, "J.D. Salinger", "Coming of Age", 1951, "The Catcher in the Rye" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
