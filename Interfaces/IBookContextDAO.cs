90f24bb6d7fc736bc94cb8fa745f104b27d8b9b6
noahwardega <130424011+noahwardega@users.noreply.github.com>
2023-12-03 21:06:12 -0500

    added book interface

---
+++ Interfaces/IBookContextDAO.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.Collections.Generic;
+using IT3045_Final.Models;
+
+namespace IT3045_Final.Interfaces
+{
+    public interface IBookContextDAO
+    {
+        List<Book> GetAllBooks();
+        Book GetBookById(int id);
+        Book RemoveBookById(int id);
+        Book AddBook(Book book);
+        Book UpdateBook(Book book);
+    }
+}
+
